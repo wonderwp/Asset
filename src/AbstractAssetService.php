7018a37714ea22a3dfb978c5817c7833e93fc9a2
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2018-07-31 13:44:22 +0200

    Abstract asset service extends abstract service

--- src/AbstractAssetService.php
+++ src/AbstractAssetService.php
@@ -2,10 +2,15 @@
 
 namespace WonderWp\Component\Asset;
 
-abstract class AbstractAssetService implements AssetServiceInterface
+use WonderWp\Component\Service\AbstractService;
+
+abstract class AbstractAssetService extends AbstractService implements AssetServiceInterface
 {
     const PUBLIC_ASSETS_GROUP = 'app';
     const ADMIN_ASSETS_GROUP  = 'admin';
 
     protected $assets = [];
+
+    static $assetClassName = Asset::class;
+
 }
